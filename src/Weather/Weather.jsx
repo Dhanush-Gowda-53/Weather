0e33eb3d6fadbb56cae5c60adc14230211767178
Dhanush-Gowda-53 <dhanushdg5305@gmail.com>
2024-11-02 19:04:54 +0530

    All done

--- src/Weather/Weather.jsx
+++ src/Weather/Weather.jsx
@@ -9,6 +9,7 @@ const Weather = () => {
   let [weatherData, setWeatherData] = useState(null)
   let [city, setCity] = useState("")
   let [detail, setdetail] = useState(false)
+  let [found,setFound]=useState(false)
   let fetchApi = async () => {
     let apikey = "9d4afb65075749db58867ab7a3aac856"
     let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
@@ -18,8 +19,10 @@ const Weather = () => {
       console.log(finalData)
       if (finalData.cod === 200) {
         setWeatherData(finalData)
+        setFound(true)
       } else {
         setWeatherData(null)
+        setFound(false)
       }
     } catch (error) {
       console.log(error, apiUrl)
@@ -45,13 +48,13 @@ const Weather = () => {
             <p>Wind</p>
             <div className='indet'>
             <h5>Speed: <span>{weatherData.wind.speed} km/hr</span></h5>
-            <h5>Gust: <span>{weatherData.wind.gust} km/hr</span></h5>
+            <h5>Gust:{ weatherData.wind.gust?<span>{weatherData.wind.gust} km/hr</span>: <span>---</span>} </h5>
             <h5>Degree: <span>{weatherData.wind.deg} degrees</span></h5>
             </div>
           </div>
         </div> : <></>}
         <button onClick={()=>(detail ? setdetail(false) : setdetail(true))} className='detBut'>{detail ? <FaCaretUp /> : <FaCaretDown />}</button>
-      </div>): <div className='weatherCarderr'><h3>City not found</h3></div>}
+      </div>): found && <div className='weatherCarderr'><h3>City not found</h3></div>}
     </div>
   )
 }
